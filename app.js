58c182de0e9f473338c4217b63f6b1614a6aaf15
misProyectosPropios <inakifrutos@outlook.com>
2024-08-04 16:06:24 -0300

    UPDATE ./app,js: CREATE server

---
+++ app.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+const express = require('express')
+
+const app = express()
+
+//Middleware
+app.use(body_parser.urlencoded({ extended: false })) //Para poder acceder a los parametros con POST, por medio del req.body[nombre_del-parametro]
+
+app.use(express.static('public')) //Para enviar el archivo js.js, el css y las imagines
+
+app.use(function middleware(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip + " - " + req.url)
+    console.log()
+    next()
+}) 
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
